d43634602ab040c435ad6ca8e81ef7a82a49064a
hamzathul <83232962+hamzathul@users.noreply.github.com>
2024-09-16 07:34:27 +0000

    empty node project

--- script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(event){
-    event.preventDefault(); // Prevent the form from submitting in the traditional way
-    $('#ignismyModal').modal('show'); // Show the modal
-});
